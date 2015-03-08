fe287a11d8ec4401a7a88d78631b51e16edf7f78
utahta <labs.ninxit@gmail.com>
2015-03-08 23:19:38 +0900

    v0.16

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from setuptools import setup, find_packages
-VERSION = "0.15"
+VERSION = "0.16"
 
 README = os.path.join(os.path.dirname(__file__),'PKG-INFO')
 long_description = open(README).read() + "\n"
